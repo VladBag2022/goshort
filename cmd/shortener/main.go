07a182516c1598ccb0ba7c569ca1824386c3fa65
Vladislav Bag <evgeng49@yandex.ru>
2022-04-25 18:17:45 +0300

    Remove log.Fatal()

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,16 +2,16 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/VladBag2022/goshort/internal/server"
 	"github.com/VladBag2022/goshort/internal/shortener"
 	"github.com/VladBag2022/goshort/internal/storage"
-	"log"
 )
 
 func main() {
 	config, err := server.NewConfig()
 	if err != nil{
-		log.Fatal(err)
+		fmt.Println(err)
 		return
 	}
 	var memoryRepository *storage.MemoryRepository
