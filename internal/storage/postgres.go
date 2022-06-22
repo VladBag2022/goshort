d3d167c9582d30388b3dc65c1a592634e0d8c6b2
Vladislav Bag <evgeng49@yandex.ru>
2022-06-22 11:38:23 +0300

    Add sql stmt again

--- internal/storage/postgres.go
+++ internal/storage/postgres.go
@@ -13,6 +13,8 @@ type PostgresRepository struct {
 	database 		*sql.DB
 	shortenFn 		func(*url.URL) (string, error)
 	registerFn 		func() string
+	insertURLStmt 	*sql.Stmt
+	bindStmt      	*sql.Stmt
 }
 
 func NewPostgresRepository(
@@ -25,10 +27,20 @@ func NewPostgresRepository(
 	if err != nil {
 		return nil, err
 	}
+	insertURLStmt, err := db.PrepareContext(ctx, "INSERT INTO shortened_urls (id, url) VALUES ($1, $2)")
+	if err != nil {
+		return nil, err
+	}
+	bindStmt, err := db.PrepareContext(ctx, "INSERT INTO users_url_m2m (user_id, url_id) VALUES ($1, $2)")
+	if err != nil {
+		return nil, err
+	}
 	var p = &PostgresRepository{
 		database: 	db,
 		shortenFn: 	shortenFn,
 		registerFn: registerFn,
+		insertURLStmt: insertURLStmt,
+		bindStmt:      bindStmt,
 	}
 	err = p.createSchema(ctx)
 	return p, err
@@ -37,7 +49,17 @@ func NewPostgresRepository(
 func (p *PostgresRepository) Close() []error {
 	var errs []error
 
-	err := p.database.Close()
+	err := p.insertURLStmt.Close()
+	if err != nil {
+		errs = append(errs, err)
+	}
+
+	err = p.bindStmt.Close()
+	if err != nil {
+		errs = append(errs, err)
+	}
+
+	err = p.database.Close()
 	if err != nil {
 		errs = append(errs, err)
 	}
