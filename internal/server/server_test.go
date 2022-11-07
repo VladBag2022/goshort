4c2e82d8169e8eee47183be92036108b8c86b3fd
Vladislav Bag <evgeng49@yandex.ru>
2022-11-07 10:24:49 +0300

    change port and add sleep to ExampleServer

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/go-resty/resty/v2"
 	log "github.com/sirupsen/logrus"
@@ -11,13 +12,12 @@ import (
 )
 
 func ExampleServer() {
-	const url string = "http://localhost:8080"
-
 	cfg, err := NewConfig()
 	if err != nil {
 		log.Error(fmt.Sprintf("Unable to read configuration from environment variables: %s", err))
 		return
 	}
+	cfg.Address = "localhost:51515"
 
 	mem := storage.NewMemoryRepository(
 		misc.Shorten,
@@ -29,10 +29,12 @@ func ExampleServer() {
 		app.ListenAndServe()
 	}()
 
+	time.Sleep(time.Second) // let server start
+
 	client := resty.New()
 	resp, err := client.R().
 		EnableTrace().
-		Get(url + "/ping")
+		Get("http://" + cfg.Address + "/ping")
 	if err != nil {
 		log.Errorf("failed to run ping request. %s", err)
 		return
